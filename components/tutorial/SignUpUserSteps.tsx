c5ebf887b94a2b2071797f3302b20a6aa4eb1b59
Kaizin <kaiolucas1812@gmail.com>
2024-07-21 17:51:14 -0300

    troquei as benditas aspas

--- components/tutorial/SignUpUserSteps.tsx
+++ components/tutorial/SignUpUserSteps.tsx
@@ -13,7 +13,7 @@ export default function SignUpUserSteps() {
           >
             Login
           </Link>{" "}
-          page and sign up your first user. It's okay if this is just you for
+          page and sign up your first user. It&apos;s okay if this is just you for
           now. Your awesome idea will have plenty of users later!
         </p>
       </Step>
