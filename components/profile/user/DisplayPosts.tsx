b534a2ae7b459c509c3048491afc0f0fdeab3355
Kaizin <kaiolucas1812@gmail.com>
2024-07-22 19:41:24 -0300

    logica de avisar se user nao tem nada publicado

--- components/profile/user/DisplayPosts.tsx
+++ components/profile/user/DisplayPosts.tsx
@@ -13,7 +13,8 @@ import UserCardsContainer from "@/components/posts/userCards";
 export default function DisplayPosts({ user }: { user: any }) {
     const supabase = createClient();
     const [loading, setLoading] = useState(true);
-    const [posts, setPosts] = useState<any[]>([]);
+    const [posts, setPosts] = useState<any[]| null>([]);
+    const [noPosts, setNoPosts] = useState(false);
 
     const getPosts = useCallback(async () => {
         try {
@@ -23,15 +24,17 @@ export default function DisplayPosts({ user }: { user: any }) {
                 .from("posts")
                 .select()
                 .eq("author_id", user[0]?.id)
+                .eq("public", true)
                 .order('updated_at', { ascending: false })
 
             if (error && status !== 406) {
                 console.log(error);
                 throw error;
             }
-            if (data) {
+            if (data?.length === 0) {
+                setNoPosts(true);
+            } else {
                 setPosts(data);
-                console.log(data);
             }
 
         } catch (error) {
@@ -48,8 +51,23 @@ export default function DisplayPosts({ user }: { user: any }) {
     const router = useRouter();
 
     return (
-        <div className="flex-1 w-full flex flex-col mr-8 items-center">
-            <UserCardsContainer posts={posts} />
+        <div className="flex-1 w-full flex flex-col mr-8 mb-8 items-center">
+            <div className="w-full flex flex-col items-center">
+                <h1 className="text-2xl font-bold mt-12 mb-4">Posts</h1>
+                <div className="w-full flex flex-col items-center">
+                    {loading ? (
+                        <LoadingSm />
+                    ) : noPosts ? (
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-2xl font-bold mt-12 mb-4">
+                                Nenhum post ainda...
+                            </h1>
+                        </div>
+                    ) : (
+                        <CardsContainer posts={posts} />
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
